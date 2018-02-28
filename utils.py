c5a46a6e376a9b4a4a5ba2968f95a2c45274c676
saoyan <yanyiqinwpu@gmail.com>
2018-02-28 14:36:47 +0800

    ipdate

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def weights_init_kaiming(m):
         nn.init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
     elif classname.find('BatchNorm') != -1:
         # nn.init.uniform(m.weight.data, 1.0, 0.02)
-        m.weight.data = torch.FloatTensor(m.weight.data.size()).normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
+        m.weight.data.normal_(mean=0, std=math.sqrt(2./9./64.)).clamp_(-0.025,0.025)
         nn.init.constant(m.bias.data, 0.0)
 
 def batch_PSNR(img, imclean, data_range):
